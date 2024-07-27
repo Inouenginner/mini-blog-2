2d693c9f5682de7e8c49d69c89c4dec32d891ab4
slidingman <pssy5xfd@gmail.com>
2024-07-27 15:05:52 +0900

    add log

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,4 @@
 export function GET(request: Request) {
-    return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+    return new Response(`Hello from vercel`);
+    // return new Response(`Hello from ${process.env.VERCEL_REGION}`);
   }
