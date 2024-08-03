28fd17b430872ccd796643580a60c06defa28007
slidingman <pssy5xfd@gmail.com>
2024-08-04 03:31:53 +0900

    投稿作成処理を直す

--- api/blog.ts
+++ api/blog.ts
@@ -55,21 +55,28 @@ export async function GET(req: Request) {
   }
 }
 
-export async function POST(req: Request) {
-  const url = new URL(req.url);
-  const params = new URLSearchParams(url.search);
-  const page = parseInt(params.get("page") || "1"); // 文字列 "Jonathan" です
-  const limit = parseInt(params.get("limit") || "20");
-  const userId = "1";
-
+export async function POST(req: Request, res: VercelResponse) {
+  const rawBody = await req.text();
+  const obj = JSON.parse(rawBody);
   try {
-    const result = await getPaginatedBlogs(userId, page, limit);
-    return new Response(JSON.stringify(result), {
-      status: 201,
-      headers: { "Content-Type": "application/json" },
+    await prisma.blog.create({
+      data: {
+        title: obj.title,
+        content: obj.content,
+        userId: obj.userId,
+      },
     });
+    return new Response(
+      JSON.stringify({
+        message: "ブログが正常に作成されました",
+      }),
+      {
+        status: 201,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   } catch (error) {
-    console.error("ブログ取得中にエラーが発生しました:", error);
+    console.error("ブログ作成中にエラーが発生しました:", error);
     return new Response(JSON.stringify({ error: "サーバーエラーが発生しました" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
