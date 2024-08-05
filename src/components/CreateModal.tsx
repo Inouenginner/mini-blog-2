bc575b6f9cb1e2adc0192cfa7067e253f6b549cb
slidingman <pssy5xfd@gmail.com>
2024-08-05 20:46:25 +0900

    rows再調整

--- src/components/CreateModal.tsx
+++ src/components/CreateModal.tsx
@@ -53,7 +53,7 @@ export const CreateModal: React.FC<CreateModalProps> = ({
                   id="content"
                   value={content}
                   onChange={(e) => setContent(e.target.value)}
-                  rows={16}
+                  rows={8}
                   maxLength={21845}
                   className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                   required
